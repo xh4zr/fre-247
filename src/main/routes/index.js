560254a98382cc533c2ff1cd5d3a32b6f6b78b79
Evan Anderson <evan.anderson@caveon.com>
2019-06-18 12:09:46 -0600

    feat: basic city page

--- src/main/routes/index.js
+++ src/main/routes/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { message: 'Hello' });
+  res.render('index', { title: 'Florida Real Estate 24/7' });
+});
+
+router.get('/city/:city', function(req, res, next) {
+  const city = req.params.city;
+  res.render('city', { city });
 });
 
 module.exports = router;
